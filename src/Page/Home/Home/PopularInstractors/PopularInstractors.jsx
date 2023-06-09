5b453448443820c4b51b33848801ddc687d55d2e
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-10 03:19:43 +0600

    made popular instructor section

--- src/Page/Home/Home/PopularInstractors/PopularInstractors.jsx
+++ src/Page/Home/Home/PopularInstractors/PopularInstractors.jsx
@@ -1,30 +1,35 @@
-
 import SectionTitle from "../../../../Component/SectionTitle/SectionTitle";
 import useAuth from "../../../../hooks/useAuth";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
+import PopularInstructorCard from "./PopularInstructorCard";
 
 const PopularInstractors = () => {
-const {user}=useAuth();
+  const { user } = useAuth();
 
-const {data:instructors=[]}=useQuery({
-    queryKey:['instructors',user?.email],
-    queryFn:async ()=>{
-        const res=await axios.get(`http://localhost:5000/instructors`)
-        return res.data;
-    }})
+  const { data: instructors = [] } = useQuery({
+    queryKey: ["instructors", user?.email],
+    queryFn: async () => {
+      const res = await axios.get(`http://localhost:5000/instructors`);
+      return res.data;
+    },
+  });
 
-console.log(instructors);
-   
-    return (
-        <div>
-                <SectionTitle header="Popular Instructors "></SectionTitle>
-            
+  console.log(instructors);
 
-          
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle header="Popular Instructors "></SectionTitle>
+      <div className="grid md:grid-cols-3 gap-10">
+        {instructors.slice(0, 6).map((instructor) => (
+          <PopularInstructorCard
+            key={instructor?.key}
+            instructor={instructor}
+          ></PopularInstructorCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularInstractors;
