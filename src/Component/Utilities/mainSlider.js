9d542af4cdbef3aae00c5894e0acbb951b3e57dc
Rayhan Shorker <raihanshorker56@gmail.com>
2024-01-13 18:28:29 +0600

    chnge img url

--- src/Component/Utilities/mainSlider.js
+++ src/Component/Utilities/mainSlider.js
@@ -1,32 +1,33 @@
 const mainSlider = [
     {
       id: 1,
-      bg: "/public/slider1.avif",
-      title: "Welcome To  Our\n Sports Training Camp",
+    //   bg: "/public/slider1.avif",
+      bg: "https://i.ibb.co/QdVfrPL/slider1.jpg",
+      title: "Welcome To  Our\n Sports Trainig Camp",
       des:"We Provide you a great training in different Sports \n Explore our website to learn more about our training programs, coaching staff, facilities, and registration details.\n We look forward to welcoming you to our Sports Training Camp family!"
     },
     {
       id: 2,
-      bg: "/public/slider2.avif",
+      bg: "https://i.ibb.co/K57VYFB/slider2.jpg",
       title: "It IS The Best \n Sports Club",
       des:"We Provide you a great training in different Sports \n Explore our website to learn more about our training programs, coaching staff, facilities, and registration details.\n We look forward to welcoming you to our Sports Training Camp family!"
     },
     {
       id: 3,
-      bg: "/public/slider3.avif",
-      title: "Welcome To  Our\n Sports Training Camp",
+      bg: "https://i.ibb.co/N3CxZts/slider3.jpg",
+      title: "Welcome To  Our\n Sports Trainig Camp",
       des:"We Provide you a great training in different Sports \n Explore our website to learn more about our training programs, coaching staff, facilities, and registration details.\n We look forward to welcoming you to our Sports Training Camp family!"
     },
     {
       id: 4,
-      bg: "/public/slider4.avif",
+      bg: "https://i.ibb.co/GH4Dpwn/slider4.jpg",
       title: "It IS The Best \n Sports Club",
       des:"We Provide you a great training in different Sports \n Explore our website to learn more about our training programs, coaching staff, facilities, and registration details.\n We look forward to welcoming you to our Sports Training Camp family!"
     },
     {
       id: 5,
-      bg: "/public/slider5.jpg",
-      title: "Welcome To  Our\n Sports Training Camp",
+      bg: "https://i.ibb.co/V3dsJ61/football.jpg",
+      title: "Welcome To  Our\n Sports Trainig Camp",
       des:"We Provide you a great training in different Sports \n Explore our website to learn more about our training programs, coaching staff, facilities, and registration details.\n We look forward to welcoming you to our Sports Training Camp family!"
     },
   ];
